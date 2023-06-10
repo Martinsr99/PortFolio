960736c1d3910eb8dd614fc4c4ff21f22e312798
Martinsr99 <silesreche.martin@gmail.com>
2023-06-10 23:00:01 +0200

    feat: Add about routing

--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -1,7 +1,6 @@
 import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { ContactComponent } from './contact/contact.component';
-import { AppComponent } from '../app.component';
 import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
