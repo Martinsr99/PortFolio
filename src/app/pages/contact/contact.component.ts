ce4cd6b9406d9aa1d6392319f3ceb5c7a7758890
Martinsr99 <silesreche.martin@gmail.com>
2023-06-11 19:30:50 +0200

    feat: Add validators in form

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -5,31 +5,42 @@ import { ContactService } from 'src/app/services/contact.service';
 import Swal from 'sweetalert2';
 @Component({
   selector: 'app-contact',
-  templateUrl: './contact.component.html'
+  templateUrl: './contact.component.html',
 })
 export class ContactComponent implements OnInit {
   public contactForm: FormGroup;
-  public name: string
-  public email: string
-  public body: string
-
+  public name: string;
+  public email: string;
+  public body: string;
 
   constructor(
-    private fb: FormBuilder, private contactService: ContactService
+    private fb: FormBuilder,
+    private contactService: ContactService
   ) {}
 
   ngOnInit(): void {
     this.contactForm = this.fb.group({
       name: [this.name, Validators.required],
       email: [this.email, [Validators.required, Validators.email]],
-      body:[this.body, Validators.required],
+      body: [this.body, Validators.required],
     });
   }
 
   onSubmit(contactForm) {
-    this.contactService.SendEmail(contactForm).subscribe(resp => {
-      console.log(resp)
-    })
+    const nameControl = this.contactForm.get('name');
+    const emailControl = this.contactForm.get('email');
+    const bodyControl = this.contactForm.get('body');
+    if (nameControl.invalid) {
+      Swal.fire('Error','Name is invalid','error')
+    }
+    if (emailControl.invalid) {
+      Swal.fire('Error','Email is invalid','error')
+    }
+    if (bodyControl.invalid) {
+      Swal.fire('Error','Body is invalid','error')
+    }
+    this.contactService.SendEmail(contactForm).subscribe((resp) => {
+      console.log(resp);
+    });
   }
-
 }
