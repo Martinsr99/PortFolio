7544fc46a398f55639511db5d6ad859a5b819f38
Martinsr99 <silesreche.martin@gmail.com>
2023-06-14 16:47:17 +0200

    feat: Add first carrousel version

--- src/app/pages/technologies/technologies.component.ts
+++ src/app/pages/technologies/technologies.component.ts
@@ -1,9 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-technologies',
-  templateUrl: './technologies.component.html'
+  templateUrl: './technologies.component.html',
 })
 export class TechnologiesComponent {
+  @ViewChild('carouselContainer') carouselContainer: ElementRef;
 
+  onClickImage(imageIndex: number): void {
+    this.updateCarousel(imageIndex);
+  }
+  moveToPrevious(): void {
+    const carouselImages = this.carouselContainer.nativeElement.querySelectorAll('.carousel-image');
+    const centerImage = this.carouselContainer.nativeElement.querySelector('.carousel-image.center');
+  
+    let currentIndex = Array.from(carouselImages).indexOf(centerImage);
+  
+    if (currentIndex === 0) {
+      currentIndex = carouselImages.length - 1;
+    } else {
+      currentIndex--;
+    }
+  
+    this.updateCarousel(currentIndex);
+  }
+  
+  moveToNext(): void {
+    const carouselImages = this.carouselContainer.nativeElement.querySelectorAll('.carousel-image');
+    const centerImage = this.carouselContainer.nativeElement.querySelector('.carousel-image.center');
+  
+    let currentIndex = Array.from(carouselImages).indexOf(centerImage);
+  
+    if (currentIndex === carouselImages.length - 1) {
+      currentIndex = 0;
+    } else {
+      currentIndex++;
+    }
+  
+    this.updateCarousel(currentIndex);
+  }
+  
+  updateCarousel(centerIndex: number): void {
+    const carouselImages = this.carouselContainer.nativeElement.querySelectorAll('.carousel-image');
+  
+    carouselImages.forEach((image, index) => {
+      if (index === centerIndex) {
+        image.classList.add('center');
+      } else {
+        image.classList.remove('center');
+      }
+    });
+  }
+  
 }
