66f16c46798c318e26cecfdac9694cdc690cd9c8
Martinsr99 <silesreche.martin@gmail.com>
2023-06-27 18:30:33 +0200

    feat: block scrolling while cardShown

--- src/app/pages/video/video.component.ts
+++ src/app/pages/video/video.component.ts
@@ -1,22 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, HostBinding, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-video',
-  templateUrl: './video.component.html'
+  templateUrl: './video.component.html',
 })
-export class VideoComponent {
-  public cardShown:boolean = true;
+export class VideoComponent implements OnInit {
+  public cardShown: boolean = true;
+
+  // Add this property to bind the class dynamically
+  @HostBinding('class.disable-scroll-y')
+  get disableScrollYClass() {
+    return this.cardShown ? 'disable-scroll' : '';
+  }
 
   disableCard() {
-    this.cardShown=false
-    this.resizeDiv()
-
+    this.cardShown = false;
+    this.resizeDiv();
+    document.body.classList.add('enable-scrollbar');
   }
+
   resizeDiv() {
     const videoElement = document.getElementById('video');
     if (videoElement) {
       videoElement.style.transform = 'scale(0.5)';
-      videoElement.style.zIndex = '999'
+      videoElement.style.zIndex = '999';
     }
+  }
+
+  // Listen for scroll events
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: Event) {
+    if (this.cardShown) {
+      // Prevent default scroll behavior on the Y-axis
+      event.preventDefault();
+      window.scrollTo(window.scrollX, 0);
+    }
+  }
+
+  ngOnInit() {
+    document.body.classList.add('disable-scrollbar');
   }
 }
